fdefbb7e028192d0e9d33a4dbc36d918a2bbd7ed
Danielboufford <daniel.boufford@gmail.com>
2017-06-22 18:35:31 -0400

    1st commit

--- static-ui/starting-point/models/index.js
+++ static-ui/starting-point/models/index.js
@@ -2,6 +2,100 @@ const db = require("./db");
 // Require your models
 // Make associations etc
 
+const Place = db.define('place', {
+	address: {
+		type: Sequelize.STRING
+	},
+	city: {
+		type: Sequelize.STRING
+	},
+	state: {
+		type: Sequelize.STRING
+	},
+	phone: {
+		type: Sequelize.STRING
+	},
+	location: {
+		type: Sequelize.ARRAY(Sequelize.FLOAT) // unsure
+	}
+});
+
+const Hotel = db.define('hotel', {
+	name: {
+		type: Sequelize.STRING
+	},
+	num_stars: {
+		type: Sequelize.FLOAT(1,5) //unsure
+	},
+	amenities: {
+		type: Sequelize.TEXT
+	}
+});
+
+const Activity = db.define('activity', {
+	name: {
+		type: Sequelize.STRING
+	},
+	age_range: {
+		type: Sequelize.STRING
+	}
+});
+
+const Restaruant = db.define('restaurant', {
+	name: {
+		type: Sequelize.STRING
+	},
+	cuisine: {
+		type: Sequelize.STRING //unsure
+	},
+	price: {
+		type: Sequelize.INT //has to be 1-5, unsure
+	}
+	}
+});
+
+Hotel.belongsTo(Place);
+Restaurant.belongsTo(Place);
+Activity.belongsTo(Place);
+
+
 module.exports = {
-  db
+  db,
+  Place,
+  Hotel,
+  Activity,
+  Restaruant
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
