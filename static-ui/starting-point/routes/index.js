c76cacaf3cf051d2651c63f9f72350debba3e4e0
Danielboufford <daniel.boufford@gmail.com>
2017-06-23 12:40:04 -0400

    3rd edit

--- static-ui/starting-point/routes/index.js
+++ static-ui/starting-point/routes/index.js
@@ -1,7 +1,27 @@
 const route = require("express").Router();
+const db = require ('../models');
+const hotel = require('../models/hotel');
+
+//console.log(db);
 
 route.get("/", (req, res) => {
-  res.render("index.html");
+	var hotelPromise = db.Hotel.findAll();
+	var restaurantPromise = db.Restaurant.findAll();
+	var activityPromise = db.Activity.findAll();	
+	var all = [hotelPromise, restaurantPromise, activityPromise];
+
+	Promise.all(all)
+	.then(results => {
+		console.log(results);
+  		res.render("index.html", {
+  			hotels: results[0],
+  			restaurants: results[1],
+  			activities: results[2]
+
+  		});	
+	})
+
+
 });
 
 route.post("/login", (req, res) => {
